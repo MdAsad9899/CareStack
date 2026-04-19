e65e843b32ab1ee1b7bb7b60fdf5955d45524239
MD ASAD <mdasadgodargama@gmail.com>
2026-04-19 23:14:25 +0530

    Today i built navbar with the hero header section day first

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Headers from '../components/Headers'
+import SpecialityMenu from '../components/SpecialityMenu'
+
+const Home = () => {
+  return (
+    <div>
+      <Headers />
+      <SpecialityMenu />
+    </div>
+  )
+}
+
+export default Home
