d8383e59f7c5333213659accbdbc20331738232b
MD ASAD <mdasadgodargama@gmail.com>
2026-04-23 00:50:53 +0530

    completed Home Pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Headers from '../components/Headers'
 import SpecialityMenu from '../components/SpecialityMenu'
+import TopDoctors from '../components/TopDoctors'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
     <div>
       <Headers />
       <SpecialityMenu />
+      <TopDoctors />
+      <Banner />
     </div>
   )
 }
